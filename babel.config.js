619909d153df1aa50393be5fb4e24ea2ee9ec8be
Brent Mundt <brmundt10@gmail.com>
2020-11-01 11:28:36 -0500

    try to fix babel config

--- babel.config.js
+++ babel.config.js
@@ -57,9 +57,9 @@ module.exports = function (api) {
 
   return {
     presets: [
-      ['@babel/preset-env'],
+      ['@babel/preset-typesscript'], // added to try to fix dev on heroku
       [
-        '@babel/preset-typesscript', // added to try to fix dev on heroku
+        '@babel/preset-env',
         {
           bugfixes: true,
           modules: false,
